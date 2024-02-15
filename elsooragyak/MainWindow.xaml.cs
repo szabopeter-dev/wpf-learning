584a6f23efe6b0dfc0f3ce1d3e9504a173bb7b43
hallgato <hallgato@LABOR113-19>
2024-02-15 13:04:49 +0100

    Add project files.

---
+++ elsooragyak/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace elsooragyak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+      
+    {
+       public static int counter = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("hello");
+            lb_barmi.Content = "hello user!";
+            pgb.Value++;
+            if (counter %2 == 0  )
+            {
+                myradio.IsChecked = true;
+            }
+            else
+            {
+                myradio.IsChecked= false;
+            }
+
+            lbbox.Items.Add(lb_barmi.Content);
+        }
+
+        private void lbbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show();
+        }
+    }
+}
